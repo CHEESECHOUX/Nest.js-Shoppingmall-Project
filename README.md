9a30b00f725ed650131c6ca463560d572fda5cff
cheesechoux <gsoosuu@gmail.com>
2023-07-10 14:57:14 +0900

    feat: joi

--- README.md
+++ README.md
@@ -3,5 +3,8 @@
 -   환경변수
     npm i --save @nestjs/config
 
--   내장 ValidationPipe 사용
+-   validationpipe (클라이언트에서 들어오는 데이터 유효성 검사)
     npm i --save class-validator class-transformer
+
+-   joi (환경 변수 유효성 검사)
+    npm install --save joi
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from '@src/app.controller';
+import { AppService } from '@src/app.service';
+import * as Joi from 'joi';
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: `${__dirname}/config/.${process.env.NODE_ENV}.env`,
+            validationSchema: Joi.object({
+                NODE_ENV: Joi.string().valid('development', 'production', 'test').default('development'),
+                PORT: Joi.number().default(3000),
+            }),
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
