60672ced9dc9d9648f79bc83bf474df2413bc687
cheesechoux <gsoosuu@gmail.com>
2023-07-17 13:08:23 +0900

    feat: category create, update, softDelete - ADMIN, MANAGER(role)

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,9 +1,13 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UseGuards } from '@nestjs/common';
 import { CategoriesService } from '@src/categories/categories.service';
 import { CategoryInfoDTO, CreateCategoryDTO } from '@src/categories/dto/categories.dto';
-import { Category } from './entity/categories.entity';
+import { Category } from '@src/categories/entity/categories.entity';
+import { JwtAuthGuard } from '@src/users/jwt/jwt.guard';
+import { RolesGuard } from '@src/guards/roles.guard';
+import { Roles } from '@src/common/decorators/role.decorator';
 
 @Controller('categories')
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class CategoriesController {
     constructor(private readonly categoriesService: CategoriesService) {}
 
@@ -19,16 +23,19 @@ export class CategoriesController {
         return categoriesInfo;
     }
 
+    @Roles('ADMIN', 'MANAGER')
     @Post()
     async createCategory(@Body() createCategoryDTO: CreateCategoryDTO) {
         return this.categoriesService.createCategory(createCategoryDTO);
     }
 
+    @Roles('ADMIN', 'MANAGER')
     @Patch(':id')
     async updateCategory(@Param('id') id: number, @Body() createCategoryDTO: CreateCategoryDTO) {
         return this.categoriesService.updateCategory(id, createCategoryDTO);
     }
 
+    @Roles('ADMIN', 'MANAGER')
     @Delete(':id')
     async softDeleteCategory(@Param('id') id: number): Promise<void> {
         return this.categoriesService.softDeleteById(id);
