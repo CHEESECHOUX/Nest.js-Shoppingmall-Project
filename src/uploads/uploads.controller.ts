0aabb95e999a7abb6b96c9a94288b8c29e2ed0f7
cheesechoux <gsoosuu@gmail.com>
2023-07-30 17:25:35 +0900

    fix: AWS S3 이미지 파일 업로드 - ADMIN, MANAGER 권한만 가능

--- src/uploads/uploads.controller.ts
+++ src/uploads/uploads.controller.ts
@@ -1,12 +1,16 @@
-import { Body, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Post, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { Roles } from '@src/common/decorators/role.decorator';
 import { UploadsService } from '@src/uploads/uploads.service';
+import { JwtAuthGuard } from '@src/users/jwt/jwt.guard';
 
 @Controller('/uploads')
+@UseGuards(JwtAuthGuard)
 export class UploadsController {
     constructor(private readonly uploadsService: UploadsService) {}
 
     @Post()
+    @Roles('ADMIN', 'MANAGER')
     @UseInterceptors(FileInterceptor('file'))
     async uploadFile(@UploadedFile() file, @Body('productId') productId: number) {
         return this.uploadsService.uploadFile(file, productId);
