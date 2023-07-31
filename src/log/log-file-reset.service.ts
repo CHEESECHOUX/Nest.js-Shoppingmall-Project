d8448635f332490e364013880f67502bdacda11d
cheesechoux <gsoosuu@gmail.com>
2023-07-31 20:54:47 +0900

    feat: winston timezone 서울

--- src/log/log-file-reset.service.ts
+++ src/log/log-file-reset.service.ts
@@ -7,7 +7,7 @@ import * as fs from 'fs-extra';
 export class LogFileResetService {
     constructor(private configService: ConfigService) {}
 
-    @Cron('0 34 19 * * *', { timeZone: 'Asia/Seoul' })
+    @Cron('0 16 20 * * *', { timeZone: 'Asia/Seoul' })
     resetLogFile() {
         const userInfoLogFileName = 'userinfo.log';
         const userInfoLogFilePath = this.configService.get<string>('LOG_FILE_PATH').replace(/([^\/]*)$/, userInfoLogFileName);
--- src/log/login.logger.ts
+++ src/log/login.logger.ts
@@ -4,6 +4,10 @@ import { Cron, CronExpression } from '@nestjs/schedule';
 import { UserInfoDTO } from '@src/users/dto/users.dto';
 import { Logger, createLogger, format, transports } from 'winston';
 import { LogFileResetService } from '@src/log/log-file-reset.service';
+import * as winston from 'winston';
+import { utilities } from 'nest-winston';
+import * as moment from 'moment-timezone';
+
 @Injectable()
 export class LoginLogger {
     private loginLogger: Logger;
@@ -18,11 +22,20 @@ export class LoginLogger {
             throw new Error('LOG_FILE_PATH 환경 변수가 설정되지 않았습니다');
         }
 
+        // timezone 서울
+        const format = winston.format.combine(
+            winston.format((info, opts) => {
+                info.timestamp = moment().tz('Asia/Seoul').format('YYYY-MM-DD HH:mm:ss');
+                return info;
+            })(),
+            utilities.format.nestLike('log', { prettyPrint: true }),
+        );
+
         const loginLogFileName = 'login.log';
         const loginLogFilePath = this.logFilePath.replace(/([^\/]*)$/, loginLogFileName);
         this.loginLogger = createLogger({
             level: 'info',
-            format: format.combine(format.timestamp(), format.json()),
+            format: format,
             transports: [new transports.File({ filename: loginLogFilePath })],
         });
 
@@ -30,7 +43,7 @@ export class LoginLogger {
         const userInfoLogFilePath = this.logFilePath.replace(/([^\/]*)$/, userInfoLogFileName);
         this.userInfoLogger = createLogger({
             level: 'info',
-            format: format.combine(format.timestamp(), format.json()),
+            format: format,
             transports: [new transports.File({ filename: userInfoLogFilePath })],
         });
         this.logFileResetService = logFileResetService;
